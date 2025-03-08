21aa6f5654b749c9e3aea6cd0995d6c470437147
Kevin Liu <102651507+kev-liuht@users.noreply.github.com>
2025-03-07 21:11:23 -0500

    Updated included files and file names

--- HW/hls_ips/tcl_scripts/build_cov_update.tcl
+++ HW/hls_ips/tcl_scripts/build_cov_update.tcl
@@ -1,8 +1,8 @@
 open_project cov_update_hls -reset
 set_top cov_update
 add_files cov_update_src/cov_update.cpp
-add_files cov_update_src/cov_update.hpp
-add_files -tb cov_update_src/tb_cov_update.cpp
+# add_files cov_update_src/cov_update.hpp
+add_files -tb cov_update_src/cov_update_tb.cpp
 open_solution "solution1"
 set_part {xc7a200tsbg484-1} -tool vivado
 create_clock -period 10 -name default
